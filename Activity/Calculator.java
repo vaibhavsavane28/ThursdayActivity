df80ea3eee7d5c0dfb1cda5ed3873361e6e6a64f
vaibhavsavane28 <100573048+vaibhavsavane28@users.noreply.github.com>
2022-11-22 12:40:03 +0530

    Add files via upload

---
+++ Activity/Calculator.java
@@ -0,0 +1,40 @@
+//Author: Vaibhav Savane
+//Code to calculate 2 numbers from a given operation
+
+package question2;
+
+import java.util.*;
+
+public class Calculator {
+	public static void main (String args[]) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a number:- ");
+		int number1 = input.nextInt();
+		
+		System.out.println("Enter a second number:- ");
+		int number2 = input.nextInt();
+		
+		System.out.println("Which operation you want to perform:- ");
+		String operation = input.next();
+		
+		switch (operation) {
+		case "*":
+			System.out.println("Your multiplication is " + (number1 * number2));
+			break;
+			
+		case "+":
+			System.out.println("Your addition is " + (number1 + number2));
+			break;
+		case "-":
+			System.out.println("Your subtraction is " + (number1 - number2));
+			break;
+		case "/":
+			System.out.println("Your division is " + (number1 / number2));
+			break;
+		default: 
+			System.out.println("Enter a valid operation from +,-,*,/");
+			break;
+		}
+	}
+}
