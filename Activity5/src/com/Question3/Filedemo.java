80660912d5afa42cd8e33961ac44decac1891b9a
vaibhavsavane28 <100573048+vaibhavsavane28@users.noreply.github.com>
2022-12-15 23:53:49 +0530

    Update Filedemo.java

--- Activity5/src/com/Question3/Filedemo.java
+++ Activity5/src/com/Question3/Filedemo.java
@@ -1,5 +1,41 @@
 package com.Question3;
 
+import java.io.*;
 public class Filedemo {
-
-}
+	
+	
+		public static void main(String args[])
+		{
+			try
+			{
+				int lines=0,chars=0,words=0;
+				int code=0;
+				FileInputStream fis = new FileInputStream("C:\\activity.txt");
+				while(fis.available()!=0)
+				{
+					code = fis.read();
+					if(code!=10)
+					chars++;
+					if(code==32)
+					words++;
+					if(code==13)
+					{
+						lines++;
+						words++;
+					}
+				}
+				System.out.println("No.of characters = "+chars);
+				System.out.println("No.of words = "+(words+1));
+				System.out.println("No.of lines = "+(lines+1));
+				fis.close();
+			}
+			catch(FileNotFoundException e)
+			{
+				System.out.println("Cannot find the specified file...");
+			}
+			catch(IOException i)
+			{
+				System.out.println("Cannot read file...");
+			}
+		}
+	}
