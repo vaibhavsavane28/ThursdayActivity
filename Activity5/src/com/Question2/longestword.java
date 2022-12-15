55ba49af113d961db880b61c1e62db60d15012cc
vaibhavsavane28 <100573048+vaibhavsavane28@users.noreply.github.com>
2022-12-15 23:51:22 +0530

    Update longestword.java

--- Activity5/src/com/Question2/longestword.java
+++ Activity5/src/com/Question2/longestword.java
@@ -1,5 +1,30 @@
 package com.Question2;
 
-public class longestword {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+ 
+public class longestword 
+{
+	public static void main(String [ ] args) throws FileNotFoundException {
+        new longestword().findLongestWords();
+   }
 
+public String findLongestWords() throws FileNotFoundException {
+
+ String longest_word = "";
+ String current;
+ Scanner sc = new Scanner(new File("C:\\activity.txt"));
+
+
+ while (sc.hasNext()) {
+    current = sc.next();
+     if (current.length() > longest_word.length()) {
+       longest_word = current;
+     }
+
+ }
+   System.out.println("\n"+longest_word+"\n");
+      return longest_word;
+      }
 }
